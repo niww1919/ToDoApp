7420ce7508e958273ec7e6f5a256c7751bddfc7f
niww <niww@ya.ru>
2019-12-13 22:45:29 +0300

    test recycle view

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,21 +1,29 @@
 package com.example.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatEditText;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.AbsListView;
+import android.util.Log;
+import android.view.View;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String DATA_KEY = "Data";
     RecyclerView recyclerView;
     SharedPreferences preferences;
     Set<String> stringSet;
+    AppCompatEditText editText;
+    AppCompatButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +31,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.rvViewToDo);
+        editText = findViewById(R.id.et);
+        button = findViewById(R.id.bottomAdd);
         preferences = getSharedPreferences("Preference", Context.MODE_PRIVATE);
+
+        stringSet = preferences.getStringSet(DATA_KEY, new HashSet<String>());
+        Log.i("Log", String.valueOf(stringSet.size()));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stringSet.add(String.valueOf(editText.getText()));
+                editText.setText("");
+
+            }
+        });
 
         /**
          loadRecycleView();
@@ -32,9 +54,14 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
-        ToDoAdapter adapter = new ToDoAdapter();
+        ToDoAdapter adapter = new ToDoAdapter(stringSet);
         recyclerView.setAdapter(adapter);
 
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        preferences.edit().putStringSet(DATA_KEY, stringSet).apply();
+    }
 }
--- app/src/main/java/com/example/todoapp/ToDoAdapter.java
+++ app/src/main/java/com/example/todoapp/ToDoAdapter.java
@@ -3,12 +3,25 @@ package com.example.todoapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
+    Set<String> textFromSet;
+    Iterator<String> iterator;
+
+
+    public ToDoAdapter(Set<String> text) {
+        this.textFromSet = text;
+
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -20,23 +33,35 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+//        iterator = textFromSet.iterator();
+//        while (iterator.hasNext()) {
+//
+//            holder.textView.setText(iterator.next());
+////            holder.linearLayout.addView(holder.textView);
+//        }
 
-        holder.setTextView("ddd");
+            holder.textView.setText(textFromSet.toString());
+
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 1;//fixme display content
+
+        return textFromSet.size();//fixme display content
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView textView;
+        LinearLayout linearLayout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             textView = itemView.findViewById(R.id.tvViewToDo);
+//            textView = new TextView(itemView.getContext());
+//            linearLayout = itemView.findViewById(R.id.ll);
         }
 
         void setTextView(String text) {
